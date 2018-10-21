cddfce08f0c46cc2fb1baa1c6a9e184995fc8ba8
Leah <leah.kappelmann@hs-furtwangen.de>
2018-10-21 15:15:51 +0200

    Aufgabe

--- Aufgabe-2/script.js
+++ Aufgabe-2/script.js
@@ -1,21 +1,206 @@
-document.addEventListener('DOMContentLoaded', function () {
-    //    let cards: string[] = ["Rot 1", "Rot 2", "Rot 3", "Rot 4", "Rot 5", "Rot 6", "Rot 7", "Rot 8", "Rot 9", "Gelb 1", "Gelb 2", "Gelb 3", "Gelb 4", "Gelb 5", "Gelb 6", "Gelb 7", "Gelb 8", "Gelb 9", "Gr�n 1", "Gr�n 2", "Gr�n 3", "Gr�n 4", "Gr�n 5", "Gr�n 6", "Gr�n 7", "Gr�n 8", "Gr�n 9", "Blau 1", "Blau 2", "Blau 3", "Blau 4", "Blau 5", "Blau 6", "Blau 7", "Blau 8", "Blau 9",];
-    let cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let handCards = [];
-    let filedCards = [];
-    //    let box: number = parseInt(prompt("Geben Sie eine Zahl zwischen 10 & 108 ein")); //number wird in string umgewandelt
-    //    //    function input(): void {
-    //        if (box >= 10 && box <= 108) {
-    //        createDiv();
-    //            alert("Viel Spa� beim Spielen");
-    //    }
-    //
-    //
-    // else {
-    //            alert("Bitte nur Werte zwischen 10 und 108 eingeben"); //Wenn Bedingung nicht erf�llt wurde
-    //            input();
-    //        }
-    //
-    //    }
-});
+var DynHTML_Uno;
+(function (DynHTML_Uno) {
+    document.addEventListener("DOMContentLoaded", (cards));
+    // Funktion deklarieren
+    function cards() {
+        //        // Ziehstapel
+        //        let c0: Cards = {
+        //            name: "0",
+        //            blue: 0,
+        //            yellow: 0,
+        //            red: 0,
+        //            green: 0
+        //        }
+        // console.log(Ziehkarten)
+        // Karten von 1 - 9
+        let c1 = {
+            name: "1",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        let c2 = {
+            name: "2",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2,
+        };
+        let c3 = {
+            name: "3",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        let c4 = {
+            name: "4",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        let c5 = {
+            name: "5",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        let c6 = {
+            name: "6",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        let c7 = {
+            name: "7",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        let c8 = {
+            name: "8",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        let c9 = {
+            name: "9",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        };
+        // Bonuskarten
+        let c_skip = {
+            name: "Skip",
+            blue: 1,
+            red: 1,
+            green: 1,
+            yellow: 1
+        };
+        let c_draw2 = {
+            name: "Draw Two",
+            blue: 1,
+            red: 1,
+            green: 1,
+            yellow: 1
+        };
+        let c_wild = {
+            name: "Wild",
+            blue: 1,
+            red: 1,
+            green: 1,
+            yellow: 1
+        };
+        // Karten im Array speichern
+        let allCards = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c_skip, c_draw2, c_wild];
+        let s1 = 1;
+        let s2 = 1;
+        let c = "";
+        // Zufallsfunktion
+        function random1(x) {
+            return Math.floor(Math.random() * Math.floor(x));
+        }
+        ;
+        // Handkarten
+        function placeDiv(_color, _v, _y) {
+            let div = document.createElement("div");
+            document.body.appendChild(div);
+            div.setAttribute("id", "a" + _y);
+            document.getElementById("a" + _y).innerHTML += _v;
+            let s = div.style;
+            s.border = "solid white";
+            s.textAlign = "center";
+            s.fontSize = "120%";
+            s.position = "absolute";
+            s.backgroundColor = _color;
+            s.width = 8 + "%";
+            s.height = 30 + "%";
+            s.margin = "3%";
+            s.left = (_y + 0.2) * 10 + "%";
+            s.bottom = 4 + "%";
+            s.color = "white";
+        } // placeDiv Ende
+        let z;
+        // Dialogfenster
+        let input = prompt("Bitte eine Zahl angeben");
+        alert("Viel Spass beim Spielen!");
+        // Input in Nummer umwandeln
+        z = Number(input);
+        for (let d = 0; d < z; d++) {
+            let l = random1(8);
+            if (l == 6 && s1 > 0) {
+                c = "#000000";
+                s1--;
+                placeDiv(c, "draw Two", d);
+                // Beginn der Vorschleife von vorne
+                continue;
+            } // If-Schleife Ende
+            else if (l == 6 && s1 <= 0) {
+                d--;
+                continue;
+            } // Else-if Schleife Ende
+            else {
+                if (l == 5 && s2 > 0) {
+                    c = "#000000";
+                    s2--;
+                    placeDiv(c, "Wild", d);
+                    continue;
+                }
+                else if (l == 7 && s2 <= 0) {
+                    d--;
+                    continue;
+                }
+                else {
+                    // Ausf�hrung der Random-Funktion
+                    let b = random1(4);
+                    switch (b) {
+                        // Blau
+                        case 0:
+                            c = "#2B07F6";
+                            if (allCards[l].blue > 0) {
+                                placeDiv(c, allCards[l].name, d);
+                                allCards[l].blue--;
+                                continue;
+                            }
+                        // Rot
+                        case 1:
+                            c = "#F2071F";
+                            if (allCards[l].red > 0) {
+                                placeDiv(c, allCards[l].name, d); //PlaceDiv wird �bergeben mit c f�r die Farbe, der zuf�lligen Zahl l und dem Wert d, welcher den Abstand zwischen den Karten generiert
+                                allCards[l].red--;
+                                continue;
+                            }
+                        // Gr�n
+                        case 2:
+                            c = "#6DF207";
+                            if (allCards[l].green > 0) {
+                                placeDiv(c, allCards[l].name, d);
+                                allCards[l].green--;
+                                continue;
+                            }
+                        // Gelb
+                        case 3:
+                            c = "#F2B407";
+                            if (allCards[l].yellow > 0) {
+                                placeDiv(c, allCards[l].name, d);
+                                allCards[l].yellow--;
+                                continue;
+                            }
+                            else {
+                                d--;
+                                continue;
+                            }
+                    } // switch Ende
+                } // else Ende
+            } // else Ende
+        } // for Schleife Ende
+    } // Function Ende
+})(DynHTML_Uno || (DynHTML_Uno = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe-2/script.ts
+++ Aufgabe-2/script.ts
@@ -1,22 +1,254 @@
-document.addEventListener('DOMContentLoaded', function() {
+namespace DynHTML_Uno {
+    document.addEventListener("DOMContentLoaded", (cards))
 
-    //    let cards: string[] = ["Rot 1", "Rot 2", "Rot 3", "Rot 4", "Rot 5", "Rot 6", "Rot 7", "Rot 8", "Rot 9", "Gelb 1", "Gelb 2", "Gelb 3", "Gelb 4", "Gelb 5", "Gelb 6", "Gelb 7", "Gelb 8", "Gelb 9", "Grün 1", "Grün 2", "Grün 3", "Grün 4", "Grün 5", "Grün 6", "Grün 7", "Grün 8", "Grün 9", "Blau 1", "Blau 2", "Blau 3", "Blau 4", "Blau 5", "Blau 6", "Blau 7", "Blau 8", "Blau 9",];
-    let cards: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let handCards: string[] = [];
-    let filedCards: string[] = [];
-//    let box: number = parseInt(prompt("Geben Sie eine Zahl zwischen 10 & 108 ein")); //number wird in string umgewandelt
+    // Funktion deklarieren
+    function cards() {
 
-    //    //    function input(): void {
-    //        if (box >= 10 && box <= 108) {
-    //        createDiv();
-    //            alert("Viel Spaß beim Spielen");
-    //    }
-    //
-    //
-    // else {
-    //            alert("Bitte nur Werte zwischen 10 und 108 eingeben"); //Wenn Bedingung nicht erfüllt wurde
-    //            input();
-    //        }
-    //
-    //    }
-})
+        // erstelle Interface mit Karten
+        interface Cards {
+            name: string;
+            blue: number;
+            yellow: number;
+            red: number;
+            green: number;
+        }
+
+//        // Ziehstapel
+//        let c0: Cards = {
+//            name: "0",
+//            blue: 0,
+//            yellow: 0,
+//            red: 0,
+//            green: 0
+//        }
+        // console.log(Ziehkarten)
+
+        // Karten von 1 - 9
+        let c1: Cards = {
+            name: "1",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        }
+
+        let c2: Cards = {
+            name: "2",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2,
+
+        }
+
+        let c3: Cards = {
+            name: "3",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        }
+
+        let c4: Cards = {
+            name: "4",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        }
+
+        let c5: Cards = {
+            name: "5",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+
+        }
+
+        let c6: Cards = {
+            name: "6",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        }
+
+        let c7: Cards = {
+            name: "7",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        }
+
+        let c8: Cards = {
+            name: "8",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        }
+
+        let c9: Cards = {
+            name: "9",
+            blue: 2,
+            red: 2,
+            green: 2,
+            yellow: 2
+        }
+
+        // Bonuskarten
+        let c_skip: Cards = {
+            name: "Skip",
+            blue: 1,
+            red: 1,
+            green: 1,
+            yellow: 1
+        }
+
+        let c_draw2: Cards = {
+            name: "Draw Two",
+            blue: 1,
+            red: 1,
+            green: 1,
+            yellow: 1
+        }
+
+        let c_wild: Cards = {
+            name: "Wild",
+            blue: 1,
+            red: 1,
+            green: 1,
+            yellow: 1
+        }
+
+        // Karten im Array speichern
+        let allCards: Cards[] = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c_skip, c_draw2, c_wild]
+        let s1: number = 1
+        let s2: number = 1
+        let c: string = "";
+
+        // Zufallsfunktion
+        function random1(x: number) {
+            return Math.floor(Math.random() * Math.floor(x))
+        };
+
+        // Handkarten
+        function placeDiv(_color: string, _v: string, _y: number): void {
+            let div: HTMLDivElement = document.createElement("div");
+            document.body.appendChild(div);
+            div.setAttribute("id", "a" + _y);
+            document.getElementById("a" + _y).innerHTML += _v;
+            let s: CSSStyleDeclaration = div.style;
+            s.border = "solid white";
+            s.textAlign = "center";
+            s.fontSize = "120%";
+            s.position = "absolute";
+            s.backgroundColor = _color;
+            s.width = 8 + "%";
+            s.height = 30 + "%";
+            s.margin = "3%";
+            s.left = (_y + 0.2) * 10 + "%";
+            s.bottom = 4 + "%";
+            s.color = "white";
+
+
+        } // placeDiv Ende
+
+        let z: number;
+
+        // Dialogfenster
+        let input: string = prompt("Bitte eine Zahl angeben");
+        alert("Viel Spass beim Spielen!");
+
+        // Input in Nummer umwandeln
+        z = Number(input);
+
+
+        for (let d: number = 0; d < z; d++) { //Begin der For-Schleife
+            let l = random1(8);
+            if (l == 6 && s1 > 0) {
+                c = "#000000";
+                s1--;
+                placeDiv(c, "draw Two", d);
+
+                // Beginn der Vorschleife von vorne
+                continue;
+            } // If-Schleife Ende
+
+            else if (l == 6 && s1 <= 0) {
+                d--;
+                continue;
+            } // Else-if Schleife Ende
+
+            else {
+                if (l == 5 && s2 > 0) {
+                    c = "#000000";
+                    s2--;
+                    placeDiv(c, "Wild", d);
+                    continue;
+                }
+
+                else if (l == 7 && s2 <= 0) {
+                    d--;
+                    continue;
+                }
+
+                else {
+                    // Ausführung der Random-Funktion
+                    let b: number = random1(4);
+                    switch (b) {
+
+                        // Blau
+                        case 0:
+                            c = "#2B07F6";
+                            if (allCards[l].blue > 0) {
+                                placeDiv(c, allCards[l].name, d);
+                                allCards[l].blue--;
+                                continue;
+                            }
+
+
+                        // Rot
+                        case 1:
+                            c = "#F2071F";
+                            if (allCards[l].red > 0) {
+                                placeDiv(c, allCards[l].name, d); //PlaceDiv wird übergeben mit c für die Farbe, der zufälligen Zahl l und dem Wert d, welcher den Abstand zwischen den Karten generiert
+                                allCards[l].red--;
+                                continue;
+                            }
+
+                        // Grün
+                        case 2:
+                            c = "#6DF207";
+                            if (allCards[l].green > 0) {
+                                placeDiv(c, allCards[l].name, d);
+                                allCards[l].green--;
+                                continue;
+                            }
+
+                        // Gelb
+                        case 3:
+                            c = "#F2B407";
+                            if (allCards[l].yellow > 0) {
+                                placeDiv(c, allCards[l].name, d);
+                                allCards[l].yellow--;
+                                continue;
+                            }
+
+                            else {
+                                d--;
+                                continue
+                            }
+
+                    } // switch Ende
+
+                } // else Ende
+
+            } // else Ende
+
+        } // for Schleife Ende
+
+    } // Function Ende
+}
