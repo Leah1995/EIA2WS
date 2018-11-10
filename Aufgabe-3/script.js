b01dcf393cd5dcfd7e9f68e2060cd40ad304702c
Leah <leah.kappelmann@hs-furtwangen.de>
2018-11-10 16:36:23 +0100

    verschoben

--- Aufgabe-3/script.js
+++ Aufgabe-3/script.js
@@ -1,1 +1,7 @@
+var uno;
+(function (uno) {
+    let cards = ["r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "g0", "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "y0", "y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9", "p2", "p2", "w", "w", "r", "r", "s", "s"];
+    let handcards = [];
+    let filedcards = [];
+})(uno || (uno = {}));
 //# sourceMappingURL=script.js.map
---
+++ Aufgabe-3/script.ts
@@ -0,0 +1,9 @@
+namespace uno {
+
+let cards: string[] = ["r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "g0", "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "y0", "y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9", "p2", "p2","w", "w", "r", "r", "s", "s"];
+let handcards: string[] = [];
+let filedcards: string[] = [];
+    
+    
+
+}
