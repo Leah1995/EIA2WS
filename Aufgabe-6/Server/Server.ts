9f743fb48319cc6fef1fa5fc6364850ac9cecc8e
Leah <leah.kappelmann@hs-furtwangen.de>
2018-11-28 22:46:16 +0100

    Aufgabe 6 Kommentare

--- Aufgabe-6/Server/Server.ts
+++ Aufgabe-6/Server/Server.ts
@@ -1,12 +1,13 @@
-import * as Http from "http";
+import * as Http from "http"; // kreiert http Objekt im Code, dann sucht der Interpreter nach jedem möglichen Import im http-Modul und fügt ihn nacheinander dem http Objekt im Code hinzu
 
-namespace L06_SendData {
-    console.log("Starting server");
-    let port: number = process.env.PORT;
-    if (port == undefined)
-        port = 8100;
+namespace L06_SendData { // Namespace definiert die Sektion einer Lagerung oder Speicherung um Daten als Variablen, Funktionen, Arrays, Objekte usw abzuspeichern
 
-    let server: Http.Server = Http.createServer();
+    console.log("Starting server"); // Konsole gibt "Starting Server" aus
+    let port: number = process.env.PORT; // es wird eine Variable deklariert namens port und ihr wird der Typ number zugewiesen
+    if (port == undefined) // if Abfrage, bei der mit dem Vergleichsoperator untersucht wird ob port undefined ist
+        port = 8100; // wenn ja, wird port 8100 zugewiesen
+
+    let server: Http.Server = Http.createServer(); // es wird die Variable namens server vom Typ Http.Server deklariert und ihr wird Http.createServer zugewiesen, dabei wird ein Http Server Objekt erstellt
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
