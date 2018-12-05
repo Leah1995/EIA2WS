e4016e9bd213a5b5c0404c776ded655ae1ec344f
Leah <leah.kappelmann@hs-furtwangen.de>
2018-12-05 16:44:26 +0100

    Kommentare überarbeitet

--- Aufgabe-6/Server/Server.js
+++ Aufgabe-6/Server/Server.js
@@ -1,11 +1,12 @@
 "use strict";
 const Http = require("http"); // kreiert http Objekt im Code, dann sucht der Interpreter nach jedem m�glichen Import im http-Modul und f�gt ihn nacheinander dem http Objekt im Code hinzu
+// "http"-Objekte werden deklariert, das Modul HTTP wird aus der node.d.ts Datei importiert
 var L06_SendData;
 (function (L06_SendData) {
     console.log("Starting server"); // Konsole gibt "Starting Server" aus
-    let port = process.env.PORT; // es wird eine Variable deklariert namens port und ihr wird der Typ number zugewiesen
+    let port = process.env.PORT; // es wird eine Variable deklariert namens port und ihr wird der Typ number zugewiesen  und die process.env- Eigenschaft gibt ein Objekt zur�ck, das die Benutzerumgebung enth�lt 
     if (port == undefined)
-        port = 8100; // wenn ja, wird port 8100 zugewiesen
+        port = 8100; // wenn ja, wird port sicherheitshalber 8100 zugewiesen
     let server = Http.createServer(); // erstellt einen Server und die �bergebene R�ckruffunktion wird automatisch als Ereignishandler f�r das Anforderungsereignis registriert, l�sst Anwendung http requests empfangen und senden
     server.addListener("request", handleRequest); // es wird ein Listener hinzugef�gt, der bei request die Funktion handleRequest ausf�hrt
     server.addListener("listening", handleListen); // weiterer Listener, der bei listening die Funktion handleListen ausf�hrt
@@ -16,10 +17,10 @@ var L06_SendData;
     function handleRequest(_request, _response) {
         console.log("I hear voices!"); // Konsole gibt "I hear voices!" aus
         console.log(_request.url); // damit im Terminal bzw in der Konsole der Text des Nutzers auftaucht
-        _response.setHeader("content-type", "text/html; charset=utf-8"); // Parameter _response gibt den Header mit Text zur�ck
-        _response.setHeader("Access-Control-Allow-Origin", "*"); // Access-Control-Allow-Origin l�sst Nutzer wissen, ob es erlaubt ist, Zielressource zu verwenden
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // Parameter _response wird mit Wert und Name in den Header gesetzt
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //gibt an, ob die Antwort mit dem anfordernden Code des angegebenen Ursprungs gemeinsam genutzt werden kann
         _response.write(_request.url); // mit dem Parameter _response.write auf URL von _request zugreifen
-        _response.end(); // Ende Response
+        _response.end(); // Ende Response, Server bekommt gesagt, dass alles gesendet wurde und Nachricht komplett ist
     } // Ende Function handleRequest
 })(L06_SendData || (L06_SendData = {})); // Ende Namespace
 //# sourceMappingURL=Server.js.map
--- Aufgabe-6/Server/Server.ts
+++ Aufgabe-6/Server/Server.ts
@@ -1,33 +1,34 @@
 import * as Http from "http"; // kreiert http Objekt im Code, dann sucht der Interpreter nach jedem möglichen Import im http-Modul und fügt ihn nacheinander dem http Objekt im Code hinzu
+// "http"-Objekte werden deklariert, das Modul HTTP wird aus der node.d.ts Datei importiert
 
 namespace L06_SendData { // Namespace definiert die Sektion einer Lagerung oder Speicherung um Daten als Variablen, Funktionen, Arrays, Objekte usw abzuspeichern
 
     console.log("Starting server"); // Konsole gibt "Starting Server" aus
-    let port: number = process.env.PORT; // es wird eine Variable deklariert namens port und ihr wird der Typ number zugewiesen
+    let port: number = process.env.PORT; // es wird eine Variable deklariert namens port und ihr wird der Typ number zugewiesen  und die process.env- Eigenschaft gibt ein Objekt zurück, das die Benutzerumgebung enthält 
     if (port == undefined) // if Abfrage, bei der mit dem Vergleichsoperator untersucht wird ob port undefined ist
-        port = 8100; // wenn ja, wird port 8100 zugewiesen
+        port = 8100; // wenn ja, wird port sicherheitshalber 8100 zugewiesen
 
     let server: Http.Server = Http.createServer(); // erstellt einen Server und die übergebene Rückruffunktion wird automatisch als Ereignishandler für das Anforderungsereignis registriert, lässt Anwendung http requests empfangen und senden
     server.addListener("request", handleRequest); // es wird ein Listener hinzugefügt, der bei request die Funktion handleRequest ausführt
     server.addListener("listening", handleListen); // weiterer Listener, der bei listening die Funktion handleListen ausführt
     server.listen(port); // Methode, bei der Listener auf festgelegten Port kreiert wird
 
-    function handleListen(): void { // deklariere Function handleListen vom Typ void
+    function handleListen(): void { // deklariere Function handleListen vom Typ void, dadurch besitzt sie keinen Rückgabewert
         console.log("Listening"); // Konsole gibt "Listening" aus
 
     } // Ende Function handleListen
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // Parameter _request und _response wird an Function handleRequest übergeben 
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // Parameter _request und _response wird an Function handleRequest übergeben, request: eingehendes Nachrichtenobjekt, response: Antwort, die vom Server kommt
         console.log("I hear voices!"); // Konsole gibt "I hear voices!" aus
         console.log(_request.url); // damit im Terminal bzw in der Konsole der Text des Nutzers auftaucht
        
 
-        _response.setHeader("content-type", "text/html; charset=utf-8"); // Parameter _response gibt den Header mit Text zurück
-        _response.setHeader("Access-Control-Allow-Origin", "*"); // Access-Control-Allow-Origin lässt Nutzer wissen, ob es erlaubt ist, Zielressource zu verwenden
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // Parameter _response wird mit Wert und Name in den Header gesetzt
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //gibt an, ob die Antwort mit dem anfordernden Code des angegebenen Ursprungs gemeinsam genutzt werden kann
 
         _response.write(_request.url); // mit dem Parameter _response.write auf URL von _request zugreifen
 
-        _response.end(); // Ende Response
+        _response.end(); // Ende Response, Server bekommt gesagt, dass alles gesendet wurde und Nachricht komplett ist
 
     } // Ende Function handleRequest
 } // Ende Namespace
