4bcb88fb5c579d098f1db0b834defb278fec248a
Leah <leah.kappelmann@hs-furtwangen.de>
2018-12-06 13:09:01 +0100

    node

--- Aufgabe-6/Server/Server.ts
+++ Aufgabe-6/Server/Server.ts
@@ -30,11 +30,13 @@ namespace L06_SendData { // Namespace definiert die Sektion einer Lagerung oder 
         _response.setHeader("content-type", "text/html; charset=utf-8"); // Parameter _response wird mit Wert und Name in den Header gesetzt
         _response.setHeader("Access-Control-Allow-Origin", "*"); //gibt an, ob die Antwort mit dem anfordernden Code des angegebenen Ursprungs gemeinsam genutzt werden kann
 
+
+
         for (let key in query) {
             console.log(query[key]);
 
             _response.write(key + ": " + query[key] + "<br>");
-            _response.write(_request.url); // mit dem Parameter _response.write auf URL von _request zugreifen
+            //_response.write(_request.url); // mit dem Parameter _response.write auf URL von _request zugreifen
         }
 
 
