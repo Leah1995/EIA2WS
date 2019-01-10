28331e48fe4ea172fab27db6ab0d89f13979604b
Leah <leah.kappelmann@hs-furtwangen.de>
2019-01-10 11:53:20 +0100

    Platz machen

---
+++ Aufgabe-10/Aufg10.ts
@@ -0,0 +1,365 @@
+namespace Aufgabe9 {
+
+    window.addEventListener("load", init);
+    let crc2: CanvasRenderingContext2D;
+
+    function init(_event: Event): void {
+
+        let canvas: HTMLCanvasElement;
+        canvas = document.getElementsByTagName("canvas")[0];
+        console.log(canvas); // Test
+        crc2 = canvas.getContext("2d");
+        console.log(crc2); // Testen, ob es funktioniert
+        crc2.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Himmel
+        crc2.fillStyle = "#4E8FF7";
+        crc2.fillRect(0, 0, 400, 700);
+
+        // Sonne
+        crc2.beginPath();
+        crc2.arc(60, 80, 50, 0, 2 * Math.PI); // x, y, size
+        crc2.fillStyle = "#FAFB9D";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(60, 80, 30, 0, 2 * Math.PI);
+        crc2.fillStyle = "#F4F74B";
+        crc2.fill();
+
+        // Wolke
+        crc2.beginPath();
+        crc2.arc(320, 130, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(350, 120, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(380, 120, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(330, 130, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(390, 120, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(380, 110, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(310, 110, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(380, 100, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(340, 105, 20, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(540, 100, 40, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(430, 100, 40, 0, 2 * Math.PI);
+        crc2.fillStyle = "#E6EFFE";
+        crc2.fill();
+
+        // Funktionen
+//        drawBerg(300, 470);
+        drawSchnee(0, 300);
+        drawRodler1(90, 40);
+        drawSchlitten1(90, 30);
+        drawRodler2(20, -30);
+        drawSchlitten2(20, 350);
+        drawBaum1(100, 500, "#cc9966", "#44d271");
+        drawBaum2(400, 500, "#4c2e0a", "#00514c");
+        drawFlocke(800, 600);
+
+        // Schnee
+        function drawSchnee(_x: number, _y: number): void {
+            crc2.beginPath();
+            crc2.moveTo(_x + 500, _y + 250);
+            crc2.lineTo(_x, _y + 300);
+            crc2.lineTo(_x - 10, _y - 150);
+            crc2.closePath();
+            crc2.fillStyle = "#ffffff";
+            crc2.fill();
+            crc2.fillRect(0, 500, 800, 200);
+        } // Ende function drawSchnee
+
+        // Rodler
+        function drawRodler1(_x: number, _y: number): void {
+            // Kopf
+            crc2.beginPath();
+            crc2.arc(_x + 92, _y + 355, 7, 0, 2 * Math.PI);
+            crc2.fillStyle = "#fff7b5";
+            crc2.fill();
+            // Mütze
+            crc2.beginPath();
+            crc2.arc(_x + 91, _y + 353, 8, 1.9, 2 * Math.PI);
+            crc2.fillStyle = "#4BF7B2";
+            crc2.fill();
+            crc2.beginPath();
+            crc2.arc(_x + 85, _y + 346, 4, 0, 2 * Math.PI);
+            crc2.fillStyle = "#0ADA84";
+            crc2.fill();
+            // Körper
+            crc2.beginPath();
+            crc2.arc(_x + 92, _y + 370, 8, 3, 2 * Math.PI); // x, y, Größe
+            crc2.fillStyle = "#04623B";
+            crc2.fill();
+            crc2.fillStyle = "#0BF393";
+            crc2.fillRect(_x + 84, _y + 370, 15, 20); // Anfang, Höhe, Dicke, Länge
+            // Bein
+            crc2.beginPath();
+            crc2.moveTo(_x + 85, _y + 393);
+            crc2.lineTo(_x + 110, _y + 382);
+            crc2.strokeStyle = "#04623B";
+            crc2.lineWidth = 7;
+            crc2.stroke();
+            crc2.closePath();
+            crc2.beginPath();
+            crc2.moveTo(_x + 105, _y + 380);
+            crc2.lineTo(_x + 115, _y + 405);
+            crc2.strokeStyle = "#04623B";
+            crc2.lineWidth = 7;
+            crc2.stroke();
+            crc2.closePath();
+            // Arm
+            crc2.beginPath();
+            crc2.moveTo(_x + 88, _y + 370);
+            crc2.lineTo(_x + 90, _y + 393);
+            crc2.strokeStyle = "#fff7b5";
+            crc2.lineWidth = 6;
+            crc2.stroke();
+            crc2.closePath();
+        } // Ende function drawRodler
+
+        // Schlitten 1
+        function drawSchlitten1(_x: number, _y: number): void {
+            crc2.beginPath();
+            crc2.moveTo(_x + 79, _y + 400);
+            crc2.lineTo(_x + 120, _y + 420);
+            crc2.strokeStyle = "#7B5926";
+            crc2.lineWidth = 6;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+
+            crc2.beginPath();
+            crc2.moveTo(_x + 80, _y + 415);
+            crc2.lineTo(_x + 120, _y + 435);
+            crc2.strokeStyle = "#7B5926";
+            crc2.lineWidth = 6;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+
+            crc2.beginPath();
+            crc2.moveTo(_x + 112, _y + 415);
+            crc2.lineTo(_x + 112, _y + 430);
+            crc2.strokeStyle = "#7B5926";
+            crc2.lineWidth = 3;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+
+            crc2.beginPath();
+            crc2.moveTo(_x + 75, _y + 380);
+            crc2.lineTo(_x + 81, _y + 418);
+            crc2.strokeStyle = "#7B5926";
+            crc2.lineWidth = 5;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+        } // Ende function drawSchlitten1
+
+
+        // Rodler 2
+        function drawRodler2(_x: number, _y: number): void {
+            // Kopf
+            crc2.beginPath();
+            crc2.arc(_x + 32, _y + 345, 4, 0, 2 * Math.PI);
+            crc2.fillStyle = "#fff7b5";
+            crc2.fill();
+            // Mütze
+            crc2.beginPath();
+            crc2.arc(_x + 32, _y + 343, 4, 3, 2 * Math.PI);
+            crc2.fillStyle = "#F76A4B";
+            crc2.fill();
+            crc2.beginPath();
+            crc2.arc(_x + 30, _y + 338, 2, 0, 2 * Math.PI);
+            crc2.fillStyle = "#F3370B";
+            crc2.fill();
+
+            // Körper
+            crc2.beginPath();
+            crc2.fillStyle = "#F3370B";
+            crc2.fillRect(_x + 28, _y + 352, 8, 15); // Anfang, Höhe, Dicke, Länge
+
+            // Bein
+            crc2.beginPath();
+            crc2.moveTo(_x + 28, _y + 365);
+            crc2.lineTo(_x + 45, _y + 375);
+            crc2.strokeStyle = "#F76A4B";
+            crc2.lineWidth = 4;
+            crc2.stroke();
+            crc2.closePath();
+
+            // Arm
+            crc2.beginPath();
+            crc2.moveTo(_x + 32, _y + 352);
+            crc2.lineTo(_x + 32, _y + 365);
+            crc2.strokeStyle = "#fff7b5";
+            crc2.lineWidth = 3;
+            crc2.stroke();
+            crc2.closePath();
+
+            // Kragen
+            crc2.beginPath();
+            crc2.arc(_x + 32, _y + 352, 5, 3, 2 * Math.PI); // x, y, Größe
+            crc2.fillStyle = "#F76A4B";
+            crc2.fill();
+        } // Ende function drawRodler
+
+        // Schlitten 2
+        function drawSchlitten2(_x: number, _y: number): void {
+            console.log(drawSchlitten2);
+            crc2.beginPath();
+            crc2.moveTo(_x + 25, _y - 14);
+            crc2.lineTo(_x + 50, _y);
+            crc2.strokeStyle = "#AE7F36";
+            crc2.lineWidth = 3;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+
+            crc2.beginPath();
+            crc2.moveTo(_x + 25, _y - 4);
+            crc2.lineTo(_x + 50, _y + 10);
+            crc2.strokeStyle = "#AE7F36";
+            crc2.lineWidth = 3;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+
+            crc2.beginPath();
+            crc2.moveTo(_x + 45, _y - 4);
+            crc2.lineTo(_x + 45, _y + 7);
+            crc2.strokeStyle = "#AE7F36";
+            crc2.lineWidth = 2;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+
+            crc2.beginPath();
+            crc2.moveTo(_x + 20, _y - 35);
+            crc2.lineTo(_x + 25, _y - 3);
+            crc2.strokeStyle = "#AE7F36";
+            crc2.lineWidth = 3;
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+        } // Ende function drawSchlitten
+
+        // Baum 1
+        function drawBaum1(_x: number, _y: number, _trunkColor: string, _topColor: string): void {
+            // Stamm
+            crc2.beginPath();
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 15, _y + 160);
+            crc2.lineTo(_x - 15, _y + 160);
+            crc2.closePath();
+            crc2.fillStyle = _trunkColor;
+            crc2.fillRect(_x - 5, _y + 310, 8, 23);
+            // Baumkrone
+            crc2.beginPath();
+            crc2.moveTo(_x + 15, _y + 320);
+            crc2.lineTo(_x - 15, _y + 320);
+            crc2.lineTo(_x, _y + 290);
+            crc2.closePath();
+            crc2.fillStyle = _topColor;
+            crc2.fill();
+        } // Ende function drawBaum1       
+        
+        // Baum 2
+        function drawBaum2(_x: number, _y: number, _trunkColor: string, _topColor: string): void {
+            // Stamm
+            crc2.beginPath();
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 30, _y - 135);
+            crc2.lineTo(_x - 30, _y - 135);
+            crc2.fillStyle = _trunkColor;
+            crc2.fillRect(_x - 4, _y + 300, 5, 15);
+            crc2.closePath();
+            // Baumkrone
+            crc2.beginPath();
+            crc2.moveTo(_x + 10, _y + 300);
+            crc2.lineTo(_x - 10, _y + 300);
+            crc2.lineTo(_x, _y + 260);
+            crc2.closePath();
+            crc2.fillStyle = _topColor;
+            crc2.fill();
+        } // Ende function drawBaum2
+
+        // Schneeflocken
+        function drawFlocke(_x: number, _y: number): void {
+            // gerade Linie
+            crc2.beginPath();
+            crc2.moveTo(_x, _y - 12);
+            crc2.lineTo(_x, _y + 7);
+            crc2.strokeStyle = "white";
+            crc2.lineWidth = 2;
+            crc2.stroke();
+
+            // schiefe Linien
+            crc2.beginPath();
+            crc2.moveTo(_x - 7, _y + 3);
+            crc2.lineTo(_x + 7, _y - 7);
+            crc2.strokeStyle = "white";
+            crc2.lineWidth = 2;
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.moveTo(_x + 7, _y + 3);
+            crc2.lineTo(_x - 7, _y - 7);
+            crc2.strokeStyle = "white";
+            crc2.lineWidth = 2;
+            crc2.stroke();
+        } // Ende function drawFlocke
+
+        // Bäume zufällig hinstellen
+        for (let i: number = 0; i < 10; i++) {
+            let randomX: number = (Math.random() * (400 - 1)) + 1;
+            let randomY: number = (Math.random() * (250 - 200)) + 200;
+            let randomBaum1: number = Math.floor((Math.random() * 2)) + 1;
+            console.log("X is " + randomX, "Y is " + randomY, randomBaum1);
+            if (randomBaum1 == 1) {
+                drawBaum1(randomX, randomY, "#cc9966", "#44d271");
+            } else {
+                drawBaum2(randomX, randomY, "#4c2e0a", "#00514c");
+            }
+        } // Ende for Schleife
+
+        // Schneeflocken zufällig platzieren
+        for (let i: number = 0; i < 300; i++) {
+            let randomX: number = (Math.random() * (400 - 1)) + 1;
+            let randomY: number = (Math.random() * (600 - 1)) + 1;
+            let randomFlocke: number = Math.floor((Math.random() * 2)) + 1;
+            console.log("X is " + randomX, "Y is " + randomY, randomFlocke);
+            if (randomFlocke == 1) {
+                drawFlocke(randomX, randomY);
+            }
+        } // Ende for Schleife
+
+    } // Ende init Funktion
+
+} // Ende namespace
