81c31cd66050d7f4d8a16afbe48ed993d92e128d
Leah <leah.kappelmann@hs-furtwangen.de>
2019-02-16 23:38:24 +0100

    no stroke

--- Endabgabe/Schneeball.js
+++ Endabgabe/Schneeball.js
@@ -13,9 +13,6 @@ var Endabgabe;
             Endabgabe.crc2.arc(this.xP, this.yP, this.radius, 0, 2 * Math.PI, false);
             Endabgabe.crc2.fillStyle = "#FFFFFF";
             Endabgabe.crc2.fill();
-            Endabgabe.crc2.lineWidth = 2;
-            Endabgabe.crc2.strokeStyle = "#000000";
-            Endabgabe.crc2.stroke();
         } // Ende draw
     }
     Endabgabe.Schneeball = Schneeball; // Ende export class
--- Endabgabe/Schneeball.ts
+++ Endabgabe/Schneeball.ts
@@ -16,9 +16,6 @@ namespace Endabgabe {
             crc2.arc(this.xP, this.yP, this.radius, 0, 2 * Math.PI, false);
             crc2.fillStyle = "#FFFFFF";
             crc2.fill();
-            crc2.lineWidth = 2;
-            crc2.strokeStyle = "#000000";
-            crc2.stroke();
         } // Ende draw
     }  // Ende export class
 }  // Ende namespace
